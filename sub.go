5c0436cd39e88d634dd20c0dbb7af164ef35e791
duamel <plotnikov@vl.ru>
2018-12-26 09:57:02 +0700

    Add function with many replacement for one symbol

--- sub.go
+++ sub.go
@@ -28,9 +28,36 @@ func Sub(source string, replacement map[string]string) []string {
 		result = make([]string, len(rr))
 		copy(result, rr)
 	}
-	return result
+	return removeDuplicates(result)
 }
 
+func SubMultiMap(source string, replacement map[string][]string) []string {
+	result := make([]string, 0)
+	source = strings.ToLower(source)
+	for _, c := range source {
+		rr := make([]string, 0)
+		if v, ok := replacement[string(c)]; ok {
+			for _, val := range v {
+				if len(result) > 0 {
+					for _, cx := range result {
+						rr = append(rr, cx+string(c))
+						rr = append(rr, cx+val)
+					}
+				} else {
+					rr = append(rr, string(c))
+					rr = append(rr, val)
+				}
+			}
+		} else {
+			for _, cx := range result {
+				rr = append(rr, cx+string(c))
+			}
+		}
+		result = make([]string, len(rr))
+		copy(result, rr)
+	}
+	return removeDuplicates(result)
+}
 func SubCaseSensitive(source string, replacement map[string]string) []string {
 	result := make([]string, 0)
 	for _, c := range source {
@@ -52,6 +79,21 @@ func SubCaseSensitive(source string, replacement map[string]string) []string {
 		}
 		result = make([]string, len(rr))
 		copy(result, rr)
+	}
+	return removeDuplicates(result)
+}
+
+func removeDuplicates(elements []string) []string {
+	encountered := map[string]bool{}
+	var result []string
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			continue
+		} else {
+			encountered[elements[v]] = true
+			result = append(result, elements[v])
+		}
 	}
 	return result
 }
--- sub_test.go
+++ sub_test.go
@@ -33,6 +33,37 @@ var testSub = []struct {
 	},
 }
 
+var testSubMM = []struct {
+	s string
+	m map[string][]string
+	r []string
+}{
+	{
+		"a",
+		map[string][]string{
+			"a": {"b", "c"},
+		},
+		[]string{"a", "b", "c"},
+	},
+	{
+		"ab-cd",
+		map[string][]string{
+			"a": {"x", "z", "o", "m"}, "c": {"y"},
+		},
+		[]string{"ab-cd", "ab-yd", "xb-cd", "xb-yd", "zb-cd", "zb-yd", "ob-cd", "ob-yd", "mb-cd", "mb-yd"},
+	},
+	{
+		"ab-cd-ef-gh",
+		map[string][]string{"a": {"x"}, "c": {"y"}, "e": {"b"}},
+		[]string{"ab-cd-ef-gh", "ab-cd-bf-gh", "ab-yd-ef-gh", "ab-yd-bf-gh", "xb-cd-ef-gh", "xb-cd-bf-gh", "xb-yd-ef-gh", "xb-yd-bf-gh"},
+	},
+	{
+		"aA-bB",
+		map[string][]string{"a": {"x", "y"}},
+		[]string{"aa-bb", "ax-bb", "xa-bb", "xx-bb", "ya-bb", "yx-bb", "ay-bb", "xy-bb", "yy-bb"},
+	},
+}
+
 var testSubCaseSensitive = []struct {
 	s string
 	m map[string]string
@@ -69,6 +100,21 @@ func TestSub(t *testing.T) {
 	for i, ts := range testSub {
 		t.Run("Sub-"+strconv.Itoa(i), func(t *testing.T) {
 			result := Sub(ts.s, ts.m)
+			if len(result) != len(ts.r) {
+				fmt.Println(result)
+				t.Fatalf("Len of result should be %d elements got %d", len(ts.r), len(result))
+			}
+			if !equal(result, ts.r) {
+				t.Fatalf("Result isn't right. Expected: %s. Actual: %s", ts.r, result)
+			}
+		})
+	}
+}
+
+func TestSubMultiMap(t *testing.T) {
+	for i, ts := range testSubMM {
+		t.Run("SubMultiMap-"+strconv.Itoa(i), func(t *testing.T) {
+			result := SubMultiMap(ts.s, ts.m)
 			if len(result) != len(ts.r) {
 				fmt.Println(result)
 				t.Fatalf("Len of result should be %d elements got %d", len(ts.r), len(result))
